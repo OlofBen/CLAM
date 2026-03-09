d980fcfeb790a505af10bd7031cf14a58adaf998
Olof Bengtsson <ol1058be-s@student.lu.se>
2026-03-09 09:21:25 +0100

    Re-added heatmaps

--- models/builder.py
+++ models/builder.py
@@ -1,11 +1,14 @@
-from transformers import AutoModel, AutoImageProcessor, AutoProcessor
+import timm
 import torch.nn as nn
+from huggingface_hub import login
+from models.medsiglip_encoder import MedGemmaPatchEncoder
+from models.retccl import RetCCLEncoder
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from transformers import AutoImageProcessor, AutoModel, AutoProcessor
 from utils.constants import MODEL2CONSTANTS
 from utils.transform_utils import get_eval_transforms
-import timm
 
-from models.retccl import RetCCLEncoder
-from models.medsiglip_encoder import MedGemmaPatchEncoder
 
 class TimmEncoderWrapper(nn.Module):
     def __init__(self, model_name):
@@ -49,6 +52,37 @@ class HFEncoderWrapper(nn.Module):
                 return lhs[:, 0, :]
         else:
             return outputs[0]
+
+
+class UNI2Wrapper():
+
+    def __init__(self):
+        login()  # login with your User Access Token, found at https://huggingface.co/settings/tokens
+
+        # pretrained=True needed to load UNI2-h weights (and download weights for the first time)
+        timm_kwargs = {
+                    'img_size': 224,
+                    'patch_size': 14,
+                    'depth': 24,
+                    'num_heads': 24,
+                    'init_values': 1e-5,
+                    'embed_dim': 1536,
+                    'mlp_ratio': 2.66667*2,
+                    'num_classes': 0,
+                    'no_embed_class': True,
+                    'mlp_layer': timm.layers.SwiGLUPacked,
+                    'act_layer': nn.SiLU,
+                    'reg_tokens': 8,
+                    'dynamic_img_size': True
+                }
+        model = timm.create_model("hf-hub:MahmoodLab/UNI2-h", pretrained=True, **timm_kwargs)
+        self.transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+        self.model.eval()
+
+    def forward(self, x):
+        res = self.model(x)
+        print(res)
+
 
 def get_encoder(model_name, target_img_size=224, trust_remote_code = False):
     """
@@ -107,6 +141,11 @@ def get_encoder(model_name, target_img_size=224, trust_remote_code = False):
     # -----------------------------------------------------------
     # 4. UNI models
     # -----------------------------------------------------------
+    if "UNI2-h" in model:
+        model = UNI2Wrapper()
+        return model, model.transform
+
+
     if "UNI" in model_name:
         # --- UNI SPECIFIC LOADING ---
         model = TimmEncoderWrapper(model_name)
